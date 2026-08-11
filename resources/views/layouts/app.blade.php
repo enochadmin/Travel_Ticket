01eb3b28ebc92393f4cfc9f3f3068dab074c01bb
enochadmin <henokpetros11@gmail.com>
2026-08-11 10:00:35 +0300

    Add EEC logo favicon to browser tab

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'TravelPass') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/eec-logo.png') }}">
 
     <script>
         (function () {
